42cd26a17f09cb812167202ecb461394ad60c713
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-07-15 14:18:05 -0500

    [add] fuente boxing y gradiente h1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				boxing: ['Boxing', 'sans-serif'],
+				'boxing-striped': ['Boxing Striped', 'sans-serif']
+			}
+		},
 	},
 	plugins: [
 		require('tailwindcss-animated')
